31d60bd1fc838ff60c5d78ad39120b778a7c8e7c
jsdevper <outhkl9716@outlook.com>
2023-04-19 22:10:13 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+import { getServerSession } from "next-auth";
+import { handler } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session:any = await getServerSession(handler);
+  const user = session?.user;
+
+  if(!user) {
+    redirect('/auth/signin');
+  }
+
   return (
-    <h1 className="text-gray-600">Instagram</h1>
+    <section>
+      <FollowingBar/>
+      <PostList/>
+      <SideBar user={user}/>
+    </section>
   )
 }
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,7 +4,7 @@ export default function Avatar({image}: Props) {
   return (
     <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
       {/* eslint-disable-next-line @next/next/no-img-element*/}
-      <img className='rounded-full p-[0.1rem]' alt='usre profile' src={image ?? undefined} referrerPolicy="no-referrer"/>
+      <img className='rounded-full p-[0.1rem]' alt='user profile' src={image ?? undefined} referrerPolicy="no-referrer"/>
     </div>
   )
 }
